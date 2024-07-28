6c2deb53ca90b1c5b5e6fce3d4b3dabc441dd2a2
Harshida Shaily <20BCS2198@gmail.com>
2024-07-28 16:43:50 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dotnetpractice.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
